96f157da2176a2dc9b5ff9dc2106586316847944
MariaCecilia <mceciperrotta@gmail.com>
2025-11-24 00:56:21 +0100

    Update climate-trend-plot.py

--- climate-trend-plot.py
+++ climate-trend-plot.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar los datos desde el archivo CSV
+
 df = pd.read_csv('data/global-temp.csv')
 
-# Crear el gráfico de líneas
+
 plt.figure(figsize=(10, 6))
 plt.plot(df['Year'], df['Temperature_Anomaly'], marker='o', color='tomato', linewidth=2)
 
-# Personalizar el gráfico
+# Personalizo el grafico
 plt.title('Tendencia de la temperatura global (2013–2025)', fontsize=14)
 plt.xlabel('Año', fontsize=12)
 plt.ylabel('Anomalía de temperatura (°C)', fontsize=12)
 plt.grid(True)
 
-# Guardar el gráfico como imagen
+
 plt.savefig('global-temperature-trend.png')
 
-# Mostrar el gráfico
 plt.show()
